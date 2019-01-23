97374e961a07cc5636c5756465d44e03acdabfbc
Iman Toumi <reyan.toumi@gmail.com>
2019-01-23 09:57:47 +0400

    Added perspective camera transformation

---
+++ Assets/Scripts/Transformations/CameraTransformation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraTransformation : MonoBehaviour, ITransformation {
+
+    [SerializeField] private float focalLength = 1f;
+
+    public Matrix4x4 Matrix {
+        get {
+            Matrix4x4 matrix = new Matrix4x4(
+                new Vector4(focalLength, 0f, 0f, 0f),
+                new Vector4(0f, focalLength, 0f, 0f),
+                new Vector4(0f, 0f, 0f, 1f),
+                new Vector4(0f, 0f, 0f, 0f));
+            return matrix;
+        }
+    }
+}
